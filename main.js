fc51e01446fc7d1035f1cd4d4136bb636c35223d
Mykhailo Chui <mihach0493@gmail>
2020-09-15 18:21:57 +0200

    druga zmiana

--- main.js
+++ main.js
@@ -2,18 +2,13 @@ var num1 = 2;
 var num2 = 5;
 
 function multiply(num1, num2) {
-   let result = num1 * num2;
-
    if (num1 > num2) {
       console.log('jesli jest wiekszy od 5');
+      return num1;
    }
-   else (num1 < num2) {
-      console.log('jesli jest mniejsza od 5');
-   }
+   else (num1 < num2)
+   console.log('jesli jest mniejsza od 5');
    return num2;
-
-
-
 }
 
 console.log(multiply(2, 5));
